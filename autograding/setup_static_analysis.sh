d77f60c2b17d234cefab7e59a543687aa6b12984
Fool Tinkerer <fooltinkerer@gmail.com>
2024-04-07 20:02:50 +0000

    feat: added option to build cppcheck for specific version

--- autograding/setup_static_analysis.sh
+++ autograding/setup_static_analysis.sh
@@ -2,7 +2,7 @@
 
 # install needed tools
 sudo apt update
-sudo apt install -y cppcheck  cloc pip
+sudo apt install -y cppcheck  cloc pip libpcre3 libpcre3-dev cmake
 pip install lizard cpplint
 
 # print out versions
@@ -10,3 +10,11 @@ cppcheck --version
 cpplint --version
 cloc --version
 lizard --version
+
+# cd /tmp
+# git clone https://github.com/danmar/cppcheck.git
+# git checkout 2.10.3
+# mkdir build && cd build 
+# cmake -DHAVE_RULES=ON ..
+# cmake --build .
+# ./bin/cppcheck --addon=misra /tmp/misra_example/*
